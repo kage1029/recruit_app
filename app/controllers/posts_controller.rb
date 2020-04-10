c57b6f54152bbffcc3b52284f6f95340bb52bb10
kage1029 <ichika789rider@gmail.com>
2020-04-10 17:08:55 +0900

    only correct_user can RUD posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -77,7 +77,7 @@ class PostsController < ApplicationController
 
     def correct_user
       @post = current_user.posts.find_by(id: params[:id])
-      unless post
+      unless @post
         redirect_to root_url
       end
     end
